9e292718bdd42bf505b756855763bb95606383ef
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-03-14 18:02:05 +0530

    Top banner styling completed

--- src/components/TopBanner.jsx
+++ src/components/TopBanner.jsx
@@ -6,6 +6,7 @@ class TopBanner extends React.Component {
 			confirmed: 0,
 			recovered: 0,
 			deaths: 0,
+			lastUpdate: '',
 		},
 	};
 
@@ -23,6 +24,7 @@ class TopBanner extends React.Component {
 						confirmed: data[0].confirmed,
 						recovered: data[0].recovered,
 						deaths: data[0].deaths,
+						lastUpdate: data[0].lastUpdate,
 					},
 				});
 			})
@@ -32,32 +34,36 @@ class TopBanner extends React.Component {
 	render() {
 		return (
 			<div className='top-banner'>
-				<div className='heading-wrapper'>
-					<h1>Covid-19 Tracker</h1>
-					<h2>
-						Built by{' '}
-						<a href='https://linktr.ee/pulkit_jasti' target='_blank'>
-							Pulkit Jasti
-						</a>{' '}
-						| Source:{' '}
-						<a href='https://github.com/pulkit-jasti/Covid-19-Tracker' target='_blank'>
-							GitHub
-						</a>
-					</h2>
-				</div>
-				<div className='stats'>
-					<div className='stat-wrapper'>
-						<p className='numbers'>{this.state.data.confirmed}</p>
-						<p className='lable'>Total Confirmed</p>
+				<img src='https://c.files.bbci.co.uk/D505/production/_115033545_gettyimages-1226314512.jpg' alt='' />
+				<div className='overlay'>
+					<div className='heading-wrapper'>
+						<h1>Covid-19 Tracker</h1>
+						<h2>
+							Built by{' '}
+							<a href='https://linktr.ee/pulkit_jasti' target='_blank'>
+								Pulkit Jasti
+							</a>{' '}
+							| Source:{' '}
+							<a href='https://github.com/pulkit-jasti/Covid-19-Tracker' target='_blank'>
+								GitHub
+							</a>
+						</h2>
 					</div>
-					<div className='stat-wrapper'>
-						<p className='numbers'>{this.state.data.recovered}</p>
-						<p className='lable'>Total Recovered</p>
+					<div className='stats'>
+						<div className='stat-wrapper'>
+							<p className='numbers'>{this.state.data.confirmed}</p>
+							<p className='lable'>Total Confirmed</p>
+						</div>
+						<div className='stat-wrapper'>
+							<p className='numbers'>{this.state.data.recovered}</p>
+							<p className='lable'>Total Recovered</p>
+						</div>
+						<div className='stat-wrapper'>
+							<p className='numbers'>{this.state.data.deaths}</p>
+							<p className='lable'>Total Deaths</p>
+						</div>
 					</div>
-					<div className='stat-wrapper'>
-						<p className='numbers'>{this.state.data.deaths}</p>
-						<p className='lable'>Total Deaths</p>
-					</div>
+					<div className='date'>Last Updated: {this.state.data.lastUpdate}</div>
 				</div>
 			</div>
 		);

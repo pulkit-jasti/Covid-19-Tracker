a53692e2e1a29f715f473c4a3b8675e86fa8dcf3
Pulkit Jasti <pulkit.jasti@gmail.com>
2021-03-15 15:42:32 +0530

    Sorting feature added

--- src/components/CountryStats.jsx
+++ src/components/CountryStats.jsx
@@ -26,13 +26,57 @@ class CountryStats extends React.Component {
 
 	handleSearch(e) {
 		var searchValue = e.target.value;
-		console.clear();
 		this.setState(prevState => {
 			let filterList = prevState.fullList.filter(el => {
 				return el.country.search(new RegExp(searchValue, 'i')) != -1;
 			});
-			console.log(filterList);
 			return { displayList: filterList };
+		});
+	}
+
+	handleSort(e) {
+		console.log('option selected', e.target.value);
+		this.setState(prevState => {
+			return { displayList: prevState.fullList };
+		});
+		console.log('After set state');
+		let updatedList = [];
+
+		switch (e.target.value) {
+			case 'default':
+				updatedList = this.state.fullList;
+				break;
+
+			case 'C-HL':
+				updatedList = Sort(this.state.displayList, 'confirmed', 'H-L');
+				break;
+
+			case 'C-LH':
+				updatedList = Sort(this.state.displayList, 'confirmed', 'L-H');
+				break;
+
+			case 'R-HL':
+				updatedList = Sort(this.state.displayList, 'recovered', 'H-L');
+				break;
+
+			case 'R-LH':
+				updatedList = Sort(this.state.displayList, 'recovered', 'L-H');
+				break;
+
+			case 'D-HL':
+				updatedList = Sort(this.state.displayList, 'deaths', 'H-L');
+				break;
+
+			case 'D-LH':
+				updatedList = Sort(this.state.displayList, 'deaths', 'L-H');
+				break;
+
+			default:
+				break;
+		}
+
+		this.setState({
+			displayList: updatedList,
 		});
 	}
 
@@ -43,14 +87,14 @@ class CountryStats extends React.Component {
 					<div className='search-wrapper'>
 						<input type='text' placeholder='Search by Country name' onChange={this.handleSearch.bind(this)} />
 						<div className='select'>
-							<select name='sort' id=''>
-								<option value=''>Sort</option>
-								<option value=''>Confirmed: Highest to Lowest</option>
-								<option value=''>Confirmed: Lowest to Highest</option>
-								<option value=''>Recovered: Highest to Lowest</option>
-								<option value=''>Recovered: Lowest to Highest</option>
-								<option value=''>Deaths: Highest to Lowest</option>
-								<option value=''>Deaths: Lowest to Highest</option>
+							<select name='sort' id='' onChange={this.handleSort.bind(this)}>
+								<option value='default'>Sort</option>
+								<option value='C-HL'>Confirmed: Highest to Lowest</option>
+								<option value='C-LH'>Confirmed: Lowest to Highest</option>
+								<option value='R-HL'>Recovered: Highest to Lowest</option>
+								<option value='R-LH'>Recovered: Lowest to Highest</option>
+								<option value='D-HL'>Deaths: Highest to Lowest</option>
+								<option value='D-LH'>Deaths: Lowest to Highest</option>
 							</select>
 						</div>
 					</div>
@@ -71,6 +115,30 @@ class CountryStats extends React.Component {
 			</div>
 		);
 	}
+}
+
+function Sort(arr, key, operation) {
+	if (operation == 'L-H') {
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < arr.length - 1; j++) {
+				if (arr[j][key] > arr[j + 1][key]) {
+					[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+				}
+			}
+		}
+	} else if (operation == 'H-L') {
+		for (let i = 0; i < arr.length; i++) {
+			for (let j = 0; j < arr.length - 1; j++) {
+				if (arr[j][key] < arr[j + 1][key]) {
+					[arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+				}
+			}
+		}
+	} else {
+		console.error('Wrong parameter in sort function');
+	}
+
+	return arr;
 }
 
 export default CountryStats;
